940983b24d98df4b23433a6c574896820f7c4dc9
lorow <smykupyka@gmail.com>
2016-04-17 22:21:56 +0200

    added some code and level desing, some bugs still exist

---
+++ Beabest/Assets/scripts/dooropener.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dooropener : MonoBehaviour
+{
+
+    public GameObject startingPosition;
+    public GameObject endPosition;
+    public GameObject startPositionSecond;
+    public GameObject endPositionSecond;
+
+    public GameObject door1;
+    public GameObject door2;
+
+    public float time = 2; 
+      void OnTriggerEnter(Collider other)
+    {
+        door1.transform.localPosition = Vector3.Lerp(door1.transform.localPosition, endPosition.transform.localPosition, time * Time.deltaTime);
+        door2.transform.localPosition = Vector3.Lerp(door2.transform.localPosition, endPositionSecond.transform.localPosition, time * Time.deltaTime);
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        door1.transform.localPosition = Vector3.Lerp(door1.transform.localPosition, startingPosition.transform.localPosition, time * Time.deltaTime);
+        door1.transform.localPosition = Vector3.Lerp(door1.transform.localPosition, startPositionSecond.transform.localPosition, time * Time.deltaTime);
+    }
+}
